2f78dc940039c3bf0a373e85d0f4b20ffa43813f
prashanth1224 <prashanthever@gmail.com>
2016-12-14 15:56:19 -0500

    Completed desktip and mobile header styles

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,10 +2,6 @@ var gulp = require('gulp'),
 watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
-
-
-
-
 gulp.task('watch', function () {
 
 	browserSync.init({
@@ -23,9 +19,17 @@ gulp.task('watch', function () {
 		gulp.start('cssInject');
 	});
 
+	watch('./app/assets/scripts/**/*.js', function(){
+		gulp.start('scriptsRefresh');
+	})
+
 });
 
 gulp.task('cssInject', ['styles'],function(){
 	return gulp.src('./app/temp/styles/styles.css')
 	.pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function(){
+	browserSync.reload();
 });
